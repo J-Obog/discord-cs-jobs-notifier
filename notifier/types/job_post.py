a4dc913070b94dfa9b7a7edd560ecf474ed1d4f9
J-Obog <jobogbaimhe@gmail.com>
2023-06-14 23:14:12 -0400

    adding post source enum

--- notifier/types/job_post.py
+++ notifier/types/job_post.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from notifier.types.post_source import PostSource
+
 @dataclass
-class JobPosting:
+class JobPost:
     postingId: str
     companyId: str
     companyName: str
     title: str
     description: Optional[str]
+    source: PostSource
---
+++ notifier/types/post_source.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class PostSource(Enum):
+    LINKEDIN = 0
