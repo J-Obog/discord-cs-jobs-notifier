10352df5d06e0912050c0c9cc524dd1d2f87a9bd
J-Obog <jobogbaimhe@gmail.com>
2023-06-14 23:40:30 -0400

    adding notification worker

---
+++ notifier/worker/notification_worker.py
@@ -0,0 +1,20 @@
+from notifier.worker.worker import AbstractWorker
+from notifier.store.store import AbstractStore
+from notifier.posting.job_board import AbstractJobBoard
+from notifier.notify.notification_pusher import AbstractNotificationPusher
+
+class NotificationWorker(AbstractWorker):
+    def __init__(self, store: AbstractStore, job_board: AbstractJobBoard, pusher: AbstractNotificationPusher) -> None:
+        self.store = store
+        self.job_board = job_board
+        self.pusher = pusher
+
+    def work(self):
+        postings = self.job_board.get_postings()
+        
+        for post in postings:
+            if self.store.get(post.postingId) is None:
+                self.pusher.send_notification(post)
+                self.store.set(post.postingId, "1")
+
+                
