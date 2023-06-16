0971507425a856f7868a9951847d2344ad8728c4
J-Obog <jobogbaimhe@gmail.com>
2023-06-15 22:49:49 -0400

    building worker in main function

--- notifier/run.py
+++ notifier/run.py
@@ -1,9 +1,22 @@
 import dotenv
 import os
 
+from notifier.types.config import get_config
+
+from notifier.notify.discord_notifier import DiscordNotifier
+from notifier.store.redis_store import RedisStore
+from notifier.posting.linkedin import LinkedinJobBoard
+from notifier.worker.notification_worker import NotificationWorker
 
 def main():
     dotenv.load_dotenv()
+    cfg = get_config(dict(os.environ))
+
+    discord_notifier = DiscordNotifier(cfg.discord_webhook_url)
+    redis_store = RedisStore(cfg.redis_host, cfg.redis_host)
+    linkedin_board = LinkedinJobBoard(cfg.linkedin_jsession_id, cfg.linkedin_li_at)
+
+    worker = NotificationWorker(redis_store, linkedin_board, discord_notifier)
 
 
 if __name__ == "__main__":
