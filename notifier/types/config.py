49cbc212c99f56d574f8166c312caa897c69ec82
J-Obog <jobogbaimhe@gmail.com>
2023-06-15 22:44:47 -0400

    adding helper to load and return a new config

--- notifier/types/config.py
+++ notifier/types/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from typing import Dict
 
 @dataclass
 class Config:
@@ -8,3 +8,13 @@ class Config:
     linkedin_jsession_id: str
     linkedin_li_at: str
     discord_webhook_url: str
+
+
+def get_config(env_map: Dict[str, str]) -> Config:
+    return Config(
+        redis_host=env_map["REDIS_HOST"],
+        redis_port=int(env_map["REDIS_PORT"]),
+        linkedin_jsession_id = env_map["LINKEDIN_JSESSION_ID"],
+        linkedin_li_at = env_map["LINKEDIN_LI_AT"],
+        discord_webhook_url = env_map["DISCORD_WEBHOOK_URL"]
+    )
